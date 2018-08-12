35a75980ac870ed8a633bd141ad9e77a024ace62
Fernando Seguim | Cappta <fernando.seguim@cappta.com.br>
2018-08-12 17:18:37 -0300

    fix Payment Transactions Resposity Tests

--- test/Case.Transactions.Tests/Repositories/PaymentTransactionsResposityTests.cs
+++ test/Case.Transactions.Tests/Repositories/PaymentTransactionsResposityTests.cs
@@ -1,3 +1,4 @@
+using Case.Transactions.Domain.Filters;
 using Case.Transactions.Domain.Models;
 using Case.Transactions.Infra.Repositories;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,15 +11,23 @@ namespace Case.Transactions.Tests.Repositories
 	public class PaymentTransactionsResposityTests : RepositoryTests<PaymentTransaction>
 	{
 		private readonly IPaymentTransactionsRepository repository;
+		private readonly PaymentTransactionFilters filters;
 
-		public PaymentTransactionsResposityTests() : base(new List<PaymentTransaction>() { new PaymentTransaction() { MerchantCnpj = "36075268855" } }) => 
+		public PaymentTransactionsResposityTests() : base(new List<PaymentTransaction>() { new PaymentTransaction() { MerchantCnpj = "13705903000110" } })
+		{
+			this.filters = new PaymentTransactionFilters()
+			{
+				MerchantCnpjs = "13705903000110,09429293000100"
+			};
 			this.repository = new PaymentTransactionsRepository(this.Context);
+		}
+
 
 		[TestMethod]
 		[Description("When try get transaction througth filters should returns a query result with payment transaction items")]
 		public void Should_returns_a_query_result_with_payment_transaction_items()
 		{
-			var queryResult = this.repository.GetPaymentTransactions("36075268855");
+			var queryResult = this.repository.GetPaymentTransactions(this.filters);
 
 			Assert.IsTrue(queryResult.Results.Any());
 		}
